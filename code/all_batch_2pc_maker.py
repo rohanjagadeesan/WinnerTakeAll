a496323390b58ae10c5fb48efb322ec47f266682
Rohan <rohanjagadeesan2606@gmail.com>
2026-06-08 16:07:16 -0500

    changed clustering radius to 1000

--- code/all_batch_2pc_maker.py
+++ code/all_batch_2pc_maker.py
@@ -42,7 +42,7 @@ def main():
     ROOT.gROOT.SetBatch(True) #Force ROOT into headless batch mode - i.e. turn off pop up graphics
 
     # Jet definitions
-    jet_radius = 0.8 #max radius accepted by fastjet is 1000. radius value in prl paper is 0.8 in lab frame.
+    jet_radius = 1000 #max radius accepted by fastjet is 1000. radius value in prl paper is 0.8 in lab frame. Use 1000 because root data already has the clustered jets
     wta_def = fastjet.JetDefinition(fastjet.antikt_algorithm, jet_radius, fastjet.WTA_pt_scheme) #winner take all definition
     std_def = fastjet.JetDefinition(fastjet.antikt_algorithm, jet_radius) #standard E scheme definition
 
